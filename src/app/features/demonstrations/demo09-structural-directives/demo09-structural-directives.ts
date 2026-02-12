096cceada1ffa790e05b68e9bff498f0a02c423e
Quentin Geerts <geerts.quentin@gmail.com>
2026-02-12 10:13:21 +0100

    Démonstration 09 - Directives structurelles

--- src/app/features/demonstrations/demo09-structural-directives/demo09-structural-directives.ts
+++ src/app/features/demonstrations/demo09-structural-directives/demo09-structural-directives.ts
@@ -1,17 +1,26 @@
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { User } from '../../../shared/models/user.model';
 
 @Component({
   selector: 'app-demo09-structural-directives',
-  imports: [NgIf],
+  imports: [NgIf, NgFor, NgClass],
   templateUrl: './demo09-structural-directives.html',
   styleUrl: './demo09-structural-directives.css',
 })
 export class Demo09StructuralDirectives {
 
   isShowed: boolean = true;
+  users: User[] = [
+    { email: 'quentin.geerts@bstorm.be', firstname: 'Quentin', lastname: 'Geerts' },
+    { email: 'thierry.morre@cognitic.be', firstname: 'Thierry', lastname: 'Morre' },
+    { email: 'michael.person@cognitic.be', firstname: 'Michael', lastname: 'Person' },
+    { email: 'mauritcio.fivez@bstorm.be', firstname: 'Mauritcio', lastname: 'Fivez' },
+  ];
 
-  toggleShowed () {
+  users2: User[] = [];
+
+  toggleShowed() {
     this.isShowed = !this.isShowed;
   }
 
